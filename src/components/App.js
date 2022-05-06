82bef9258f75e9214614f7b1f0ddc208a982306b
i-atanasov <ivayloatanasov@zohomail.eu>
2022-05-06 11:15:26 +0300

    api req unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-    return <div className="ui container" style={{ marginTop: '20px' }}>
-        <SearchBar />
-    </div>;
-};
+class App extends React.Component {
+    state = { images: [] };
+
+    onSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+                params: { query: term }
+            });
+
+        this.setState({ images: response.data.results });
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '20px' }}>
+            <SearchBar onSubmit={this.onSubmit} />
+            Found: {this.state.images.length} images!
+            </div>
+        );
+    };
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = { searchTerm: '' };
+
+    formHandler = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.searchTerm)
+    };
+
     render() {
         return (
         <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.formHandler} className="ui form">
                 <div className='field'>
                     <label>Search form:</label>
-                    <input type='text' />
+                    <input 
+                        type='text' 
+                        value={this.state.searchTerm} 
+                        placeholder="Type search term" 
+                        onChange={ (e) => this.setState({ searchTerm: e.target.value }) } />
                 </div>
             </form>
         </div>
